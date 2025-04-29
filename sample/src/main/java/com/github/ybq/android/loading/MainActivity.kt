ad0d6780e928a2cd6ac2de2ae742cd44f7c2e89c
Sagnik <metiyasagnik692@gmail.com>
2025-04-29 11:43:33 +0530

    Merging Mainactivity.kt file ( Kotlin backend file ) as a sample test file

---
+++ sample/src/main/java/com/github/ybq/android/loading/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.github.ybq.android.spinkit.sprite.Sprite
+import com.github.ybq.android.spinkit.style.DoubleBounce
+import com.github.ybq.android.spinkit.style.Wave
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val progressBar = findViewById<View>(R.id.spin_kit) as ProgressBar
+        val wave: Sprite = Wave()
+        progressBar.indeterminateDrawable = wave
+    }
+}
